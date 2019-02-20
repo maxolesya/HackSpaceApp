cd3fd07b23070dead03f5e508127295e94537eb4
Олеся Максакова <olesa@macbook-pro-olesya.local>
2019-02-20 23:09:45 +0300

    Initial Commit

---
+++ HackSpace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackSpace
+//
+//  Created by Олеся Максакова on 20.02.2019.
+//  Copyright © 2019 HSEProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
